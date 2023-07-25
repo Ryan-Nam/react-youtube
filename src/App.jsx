be270ca8a620b14b2be0e0a760d98d97c879fce4
Ryan-Nam <alfofh7060@gmail.com>
2023-07-25 19:13:36 +1000

    Add json server

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,21 @@ import Navbar from './components/Navbar';
 import Videos from './pages/Videos';
 import SearchHeader from './components/SearchHeader';
 
+import {
+  QueryClient,
+  QueryClientProvider
+} from '@tanstack/react-query'
+
 function App() {
+  const queryClient = new QueryClient()
+
   return (
     <>
       {/* <Navbar /> */}
       <SearchHeader />
+      <QueryClientProvider client={queryClient}>
       <Outlet />
+      </QueryClientProvider>
     </>
   );
 }
--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {useParams} from 'react-router-dom';
 
 export default function Videos() {
@@ -6,10 +6,29 @@ export default function Videos() {
     const {keyword} = useParams();
     console.log(keyword);
 
+
+    const [hottrend, setHottrend] = useState('');
+
+    useEffect(()=> {
+      fetch('/videos/popular.json')
+        .then((res)=> res.json())
+        .then((data)=>setHottrend(data));
+    }, []);
+
   return (
+    <>
     <div>Video 
       {keyword ? `🔍${keyword}` : '🔥Hot Trend'}
-
     </div>
+    <div>{hottrend}</div>
+
+    {/* {hottrend.map((list) => (
+      <div key={list.id}>
+        <div>{list.items}</div>
+      </div>
+    ))} */}
+  
+    </>
+
   );
 }
