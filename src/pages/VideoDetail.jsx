dbb1d231beba968050c4395c9f33a6b426bf7fd7
Ryan Nam <alfofh7060@gmail.com>
2023-11-18 23:00:41 +1100

    Add ChannelInfo to request Thumbnail data

--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -1,23 +1,34 @@
 import React from 'react';
-import {useLocation} from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import ChannelInfo from '../components/ChannelInfo';
 
 export default function VideoDetail() {
+  const location = useLocation();
 
+  const {
+    state: { video },
+  } = useLocation();
+  console.log(video);
+console.log(video.id);
+//   console.log(location.state.video);
 
-    const location = useLocation();
+const {title, channelId, channelTitle} = video.snippet;
 
-    const {state: {video}} = useLocation();
-    // console.log(video);
+  return (
+    <>
+      <iframe
+        id='player'
+        type='text/html'
+        width='100%'
+        height='640'
+        src={`http://www.youtube.com/embed/${video.id}`}
+        frameborder='0'
+      ></iframe>
 
-    console.log(location.state.video);
-    
-    
-    return (
-        <div>
-            Video Details
-            Video name:~~
-            date: ~~
-        </div>
-    );
+      <div>
+        <h2>{title}</h2>
+        <ChannelInfo id={channelId} name={channelTitle}/>
+      </div>
+    </>
+  );
 }
-
